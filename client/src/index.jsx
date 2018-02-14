a51ef8443cd62dba547683851dbcb57552bdfdc0
Eduardo091 <urrutia.e14@gmail.com>
2018-02-14 16:18:52 -0600

    index.html finished

---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import bodyParser from 'body-parser';
+import $ from 'jquery'
+class APP extends React.Component{
+  constructor(props){
+    super(props)
+  }
+}
